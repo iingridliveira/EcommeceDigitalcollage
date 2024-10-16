937f93c03c8ec6522deaf299f5538e730262c94b
franciscojose96 <franciscojoserodrigueslins@gmail.com>
2024-10-15 21:39:36 -0300

    featureHeader

---
+++ src/components/logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+const Logo = () => {
+  return (
+    <img 
+      src={logo} 
+      alt="Logomarca" 
+      width="253" 
+      height="44" 
+    />
+  );
+};
+
+export default Logo;
