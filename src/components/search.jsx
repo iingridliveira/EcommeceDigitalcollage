910ac8e4b3babb5490b0ca603d009e5b6e24b323
franciscojose96 <franciscojoserodrigueslins@gmail.com>
2024-11-03 21:23:59 -0300

    gender

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,18 +3,21 @@ import { SearchContext } from "../context";
 import FilterCard from "./BayBox/filterCard";
 import ProductCard from "./BayBox/ProductCard";
 import CategoryCard from "./BayBox/categoryCard";
+import GenderCard from "./BayBox/gender";
 
 const SearchProducts = () => {
   const { buttonseach } = useContext(SearchContext); // Dados do contexto
   const [selectedBrands, setSelectedBrands] = useState([]);
   const [selectCategory, setSelectedCategory] = useState([]);
+  const [selectGender, setSelectedGender] = useState([]);
  
   // Filtra os produtos de acordo com as marcas selecionadas
   const filteredProducts = buttonseach.filter(
     (product) =>
       selectedBrands.length === 0 || // Exibe todos se nenhuma marca estiver selecionada
       selectedBrands.includes(product.name.split(" ")[0]) || // Filtra pela primeira palavra do nome
-      selectCategory.includes(product.category)
+      selectCategory.includes(product.category) ||
+      selectGender.includes(product.gender)
     );
     console.log(filteredProducts)
    useEffect(() => {
@@ -26,6 +29,8 @@ const SearchProducts = () => {
         <div>
           <FilterCard setSelectedBrands={setSelectedBrands} />{" "}
           <CategoryCard setSelectedCategory={setSelectedCategory} />
+          <GenderCard setSelectedGender={setSelectedGender} />
+
           {/* Componente para selecionar marcas */}
         </div>
         <div className="d-flex flex-wrap container-xxl d-md-flex">
