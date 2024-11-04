53da26cdaabc63ff5fdfb59c0f42621fa880ed47
Ingridoliveira <oligrid20@gmail.com>
2024-11-03 22:53:15 -0300

    style: texo do link do productCard sem anderline

--- src/components/BayBox/Link.jsx
+++ src/components/BayBox/Link.jsx
@@ -2,7 +2,11 @@ import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const LinkProduct = ({ index, children }) => {
-  return <Link to={`/product/${index}`}>{children}</Link>;
+  return (
+    <Link className="text-decoration-none" to={`/product/${index}`}>
+      {children}
+    </Link>
+  );
 };
 
 export default LinkProduct;
