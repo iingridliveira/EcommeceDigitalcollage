4fd016bc9a053902a74b08da610f4c335335414f
franciscojose96 <franciscojoserodrigueslins@gmail.com>
2024-11-11 23:42:26 -0300

    adicionando filter state

--- src/components/FilterGroup/state.jsx
+++ src/components/FilterGroup/state.jsx
@@ -7,10 +7,47 @@ const StateCard = ({setSelectedState}) => {
         Novo: false,
         Usado: false,
     });
-}
+
 
 const handleCheckboxChange = (ev) => {
     const { name, checked } = ev.target;
 
-    setChe
-}
+    setCheckBoxValue((prevState) == ({
+        ...prevState,
+        [name]: checked,
+    }));
+
+    // Atualiza o estado selecionado no componete pai
+
+    setSelectedState((prevState) => {
+        const newStates = checked
+        ? [...prevState, name] //Adiciona o estado se marcado
+        : prevState.filter((state) => state !== name); // Remove o estado desmarcado
+        return newStates;
+    });
+};
+
+return (
+    <main
+      className="d-flex flex-column flex-shrink-0 p-5 py-2 bg-white"
+      style={{ width: "300px" }}
+    >
+      <h6 className="pt-2">Estado</h6>
+      <form className="form-check">
+        {Object.keys(checkBoxValue).map((state) => (
+            <div key={state} className="d-flex py-2">
+                <input
+                    className="d-inline-block me-2 form-check-input"
+                    type="checkbox"
+                    name={state}
+                    checked={checkBoxValue[state]}
+                    onChange={handleCheckboxChange} />
+                <label className="form-check-label">{state}</label>
+            </div>
+        ))}
+      </form>
+    </main>
+);
+};
+
+export default StateCard;
